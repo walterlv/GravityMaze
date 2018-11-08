88f8cf65b521503c1a78200060f1c0d85d15b87d
walterlv <xblero@gmail.com>
2018-11-08 19:54:03 +0800

    Add basic Win2D Framework.

--- Walterlv.GravityMaze/MainPage.xaml.cs
+++ Walterlv.GravityMaze/MainPage.xaml.cs
@@ -12,19 +12,29 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 
 namespace Walterlv.GravityMaze
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private void OnCreateResources(CanvasAnimatedControl sender, CanvasCreateResourcesEventArgs e)
+        {
+
+        }
+
+        private void OnUpdate(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
+        {
+        }
+
+        private void OnDraw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        {
         }
     }
 }
