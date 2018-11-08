52b80e5551ad8bf9306361af620a8e5d31e05ba4
walterlv <xblero@gmail.com>
2018-11-08 21:59:53 +0800

    Add more properties.

--- Walterlv.GravityMaze/Game/Models/MazeBoard.cs
+++ Walterlv.GravityMaze/Game/Models/MazeBoard.cs
@@ -9,26 +9,33 @@ namespace Walterlv.GravityMaze.Game
     public class MazeBoard : GameObject
     {
         private readonly int[] _mazeData;
+
         public int ColumnCount { get; }
         public int RowCount { get; }
-
-        public Rect Area;
-        public float CellWidth;
-        public float CellHeight;
+        public int StartColumnIndex { get; }
+        public int StartRowIndex { get; }
+        public int EndColumnIndex { get; }
+        public int EndRowIndex { get; }
+        public Rect Area { get; private set; }
+        public float CellWidth { get; private set; }
+        public float CellHeight { get; private set; }
 
         public MazeBoard(
-            int startRowIndex, int startColumnIndex,
-            int endRowIndex, int endColumnIndex,
+            int startColumnIndex, int startRowIndex,
+            int endColumnIndex, int endRowIndex,
             params int[] mazeData)
         {
+            _mazeData = mazeData;
+
             var columns = (mazeData[0].ToString("X").Length - 1) * 2;
             var rows = mazeData.Length;
-
-
-
-            _mazeData = mazeData;
             ColumnCount = columns;
             RowCount = rows;
+
+            StartColumnIndex = startColumnIndex;
+            StartRowIndex = startRowIndex;
+            EndColumnIndex = endColumnIndex;
+            EndRowIndex = endRowIndex;
         }
 
         protected override void OnUpdate(CanvasTimingInformation timing)
